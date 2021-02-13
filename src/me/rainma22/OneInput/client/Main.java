a0a49a53613739b83c94d34469292acb0ff59e36
RainMa22 <d2rain@126.com>
2021-02-12 20:10:03 -0600

    Client Controll

--- src/me/rainma22/OneInput/client/Main.java
+++ src/me/rainma22/OneInput/client/Main.java
@@ -1,5 +1,6 @@
 package me.rainma22.OneInput.client;
 
+import java.awt.*;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
@@ -16,9 +17,27 @@ public class Main {
         Main.failsafe = failsafe;
     }
 
-
+    private static void parseString(String s) throws AWTException {
+        Robot r=new Robot();
+        if (s.contains("MX")&&s.contains("MY")){
+            int x=MouseInfo.getPointerInfo().getLocation().x;
+            int y=MouseInfo.getPointerInfo().getLocation().y;
+            for (String s1: s.split(",")) {
+                r.mouseMove(x+Integer.parseInt(s1.split(":")[1]),y+Integer.parseInt(s1.split(":")[1]));
+            }
+        }else if (s.contains("MP")){
+            r.mousePress(Integer.parseInt(s.split(":")[1]));
+        }else if (s.contains("MR")){
+            r.mouseRelease(Integer.parseInt(s.split(":")[1]));
+        }else if (s.contains("MW")){
+            r.mouseWheel(Integer.parseInt(s.split(":")[1]));
+        }else if (s.contains("KP")){
+            r.keyPress(Integer.parseInt(s.split(":")[1]));
+        }else if (s.contains("KR")){
+            r.keyPress(Integer.parseInt(s.split(":")[1]));
+        }
+    }
     public static void main(String[] args) throws Exception {
-
         DatagramSocket socket=new DatagramSocket(2020);
         byte[] b=new byte[]{0};
         DatagramPacket packet=new DatagramPacket(b,b.length, InetAddress.getByName(args[0]),2021);
